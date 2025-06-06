2b505f22efde6d75c3450c12f20cf12802379b70
shreya12345678900000 <shreyamangasuli@gmail.com>
2025-06-06 23:40:51 +0530

    Added Fibonacci1 java file

---
+++ Fibonacci1.java
@@ -0,0 +1,25 @@
+public class Fibonacci1 {
+
+    public static void generateFibonacci(int n) {
+        int first = 0, second = 1;
+        System.out.print("Fibonacci Series: " + first + ", " + second);
+
+        for (int i = 2; i < n; i++) {
+            int next = first + second;
+            System.out.print(", " + next);
+            first = second;
+            second = next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Fibonacci <number_of_terms>");
+            return;
+        }
+
+        int n = Integer.parseInt(args[0]);
+        generateFibonacci(n);
+    }
+}
